b802dfcf43a8b2c4baeb415f40f4f76563b1cd27
BuildTools <unconfigured@null.spigotmc.org>
2020-05-26 13:00:28 +0200

    Initial commit

---
+++ src/main/java/com/pmfrtt/mobarena/MobArena.java
@@ -0,0 +1,17 @@
+package com.pmfrtt.mobarena;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MobArena extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+       
+    }
+}
---
+++ src/main/java/com/pmfrtt/mobarena/MobArenaListener.java
@@ -0,0 +1,5 @@
+package com.pmfrtt.mobarena;
+
+public class MobArenaListener {
+
+}
